1d125406d6f8ee436045aae12c742adc4f6516a1
Sumant <88287698+Sumantkumarraj20@users.noreply.github.com>
2025-04-03 13:15:25 +0000

    firebaseconfig

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-// Firebase configuration
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
-  };
+  apiKey: "AIzaSyD4sWIxV-uQBUkIaBY3JcOAgMrmSdDPees",
+  authDomain: "quizeguide.firebaseapp.com",
+  projectId: "quizeguide",
+  storageBucket: "quizeguide.firebasestorage.app",
+  messagingSenderId: "688415775599",
+  appId: "1:688415775599:web:0c805d3053eef487eca69a",
+  measurementId: "G-MWZD22DB5V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
   
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
